30b6e07ef918b9503b3c2e4500f5f185fe1a4e3a
Denkinn <73609160+denkxi@users.noreply.github.com>
2024-02-15 23:43:06 +0200

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import { logo, logo1 } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo1} alt="ai_logo" className='w-36 object-contain' />
+
+        <button
+          type="button"
+          onClick={() => { window.open('') }}
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Online Article Summarizer using <br className='max-md:hidden' />
+        <span className='cyan_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Make your reading easier with <span className='font-bold'>Shoto</span>, a free tool that shortens long articles into clear and brief summaries
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
