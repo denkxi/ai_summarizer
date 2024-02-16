0f3060956ae069f29a3bee983641d7ea007fe6dd
Denkinn <73609160+denkxi@users.noreply.github.com>
2024-02-16 13:47:56 +0200

    Implemented redux, api calls and rest of functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,15 +1,68 @@
 import { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState('')
+
+  // Get article API from Redux store
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  // Save article history to local storage, so there is no reason no make duplicate calls to API
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+
+  }, []);
+
+  // Submit link
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+    }
+  }
+
+  // Copy link from story to clipboard (in case user wants to revisit the article website)
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => {
+      setCopied(false)
+    }, 3000)
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setArticle({ ...article, [name]: value });
+  }
+
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* {Search} */}
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => { }}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -20,8 +73,9 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => { }}
+            name="url"
+            value={article.url}
+            onChange={handleChange}
             required
             className="url_input peer"
           />
@@ -35,10 +89,53 @@ const Demo = () => {
         </form>
 
         {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img 
+                src={copied === item.url ? tick : copy} 
+                alt="copy-icon" 
+                className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+
+        </div>
 
       </div>
 
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Something went wrong...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">Article <span className="blue_gradient">Summary</span></h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   )
 }
